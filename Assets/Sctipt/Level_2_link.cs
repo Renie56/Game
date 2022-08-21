9a0a6158c26b5fecf959de84977a8c5015e88bc5
Romashka <rroma13579@gmail.com>
2022-08-21 23:20:27 +0000

    sixth commit

---
+++ Assets/Sctipt/Level_2_link.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_2_link : MonoBehaviour
+{
+    public void PlayGame2()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
