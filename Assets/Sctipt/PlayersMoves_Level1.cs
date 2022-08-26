36e123d41d603fa339181ffc78b49b88d8502930
Romashka <rroma13579@gmail.com>
2022-08-27 00:40:25 +0200

    eighth commit

--- Assets/Sctipt/PlayersMoves_Level1.cs
+++ Assets/Sctipt/PlayersMoves_Level1.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayersMoves_Level1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed_right_left = 5;
+    [SerializeField] private float speed_top_bottom = 1;
+    private Rigidbody2D body;
+    private Animator anim;
+
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        body.transform.rotation = Quaternion.Euler(0, 0, 0);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticallInput = Input.GetAxis("Vertical");
+        body.velocity = new Vector2(horizontalInput * speed_right_left, verticallInput * speed_top_bottom);
+
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        anim.SetBool("run", horizontalInput != 0 && verticallInput == 0);
+        anim.SetBool("up", verticallInput != 0);
     }
 }
