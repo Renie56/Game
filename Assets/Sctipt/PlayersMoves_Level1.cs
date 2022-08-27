937d70a4c69597f60883edcf5a97841613e799ec
Romashka <rroma13579@gmail.com>
2022-08-27 18:54:17 +0200

    ninth commit

--- Assets/Sctipt/PlayersMoves_Level1.cs
+++ Assets/Sctipt/PlayersMoves_Level1.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayersMoves_Level1 : MonoBehaviour
 {
-    [SerializeField] private float speed_right_left = 5;
-    [SerializeField] private float speed_top_bottom = 1;
+    /*[SerializeField] private float speed_right_left = 5;
+    [SerializeField] private float speed_top_bottom = 1;*/
+    [SerializeField]  private float speed_right_left = 3;
+    [SerializeField]  private float speed_top_bottom = 2;
     private Rigidbody2D body;
     private Animator anim;
 
@@ -29,6 +31,17 @@ public class PlayersMoves_Level1 : MonoBehaviour
         else if (horizontalInput < -0.01f)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (horizontalInput != 0 && verticallInput != 0)
+        {
+            speed_right_left = 2;
+            speed_top_bottom = 1.5f;
+        }
+        else
+        {
+            speed_right_left = 3;
+            speed_top_bottom = 2;
         }
 
         anim.SetBool("run", horizontalInput != 0 && verticallInput == 0);
