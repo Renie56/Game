eb53c179bdc4f246e2ff3a77871d785dc8f49627
Romashka <rroma13579@gmail.com>
2022-10-13 22:29:07 +0200

    fourteenth commit

--- Assets/Sctipt/Choose_level.cs
+++ Assets/Sctipt/Choose_level.cs
@@ -5,10 +5,6 @@ public class Choose_level : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
-    }
-    public void PlayGame_2()
-    {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene("Level1");
     }
 }
