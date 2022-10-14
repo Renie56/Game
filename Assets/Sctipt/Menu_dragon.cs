7a5509f857cddd0ef56f518a946d9fd8015b58c6
Romashka <rroma13579@gmail.com>
2022-10-14 23:37:52 +0200

    seventeenth commit

--- Assets/Sctipt/Menu_dragon.cs
+++ Assets/Sctipt/Menu_dragon.cs
@@ -7,6 +7,8 @@ public class Menu_dragon : MonoBehaviour
     private Rigidbody2D body;
     private Animator anim;
     private bool statement = true;
+    private bool up_down = true;
+    private float i = 0;
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
@@ -16,20 +18,57 @@ public class Menu_dragon : MonoBehaviour
     void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector3(2, body.velocity.y, 0);
-        anim.SetBool("run", body.position.x < -16f);
-        if (body.position.x > -16f)
+        if (horizontalInput > 0.01f && i < 4)
         {
-            body.velocity = new Vector3(0, body.velocity.y, 0);
-            statement = true;
-        }
-        if (horizontalInput > 0.01f)
-        {
-            Debug.Log("a");
             if (statement == true)
             {
-                statement = false;
+                if (up_down == true)
+                {
+                    i++;
+                    up_down = false;
+                    statement = false;
+                    body.velocity = new Vector3(2.5f, 0.41f, 0);
+                    anim.SetBool("run", body.position.x < 0);
+                }
+                else if (up_down == false)
+                {
+                    i++;
+                    up_down = true;
+                    statement = false;
+                    body.velocity = new Vector3(2.5f, -0.41f, 0);
+                    anim.SetBool("run", body.position.x < 0);
+                }
             }
+        }
+
+        else if (horizontalInput < -0.01f && i > 0)
+        {
+            if (statement == true)
+            {
+                if (up_down == true)
+                {
+                    i--;
+                    up_down = false;
+                    statement = false;
+                    body.velocity = new Vector3(-2.5f, 0.41f, 0);
+                    anim.SetBool("run", body.position.x < 0);
+                }
+                else if (up_down == false)
+                {
+                    i++;
+                    up_down = true;
+                    statement = false;
+                    body.velocity = new Vector3(-2.5f, -0.41f, 0);
+                    anim.SetBool("run", body.position.x < 0);
+                }
+            }
+        }
+        float i_0 = -(20f - (4f * i));
+        if (body.position.x > i_0)
+        {
+            body.velocity = new Vector3(0, 0, 0);
+            statement = true;
+            anim.SetBool("run", body.position.x < i_0);
         }
 
     }
