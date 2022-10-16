df52b6ca6841518a68f597cb319369a3dc24926b
Romashka <rroma13579@gmail.com>
2022-10-16 13:24:39 +0200

    twenty third commit

--- Assets/Sctipt/Teapot_Attack.cs
+++ Assets/Sctipt/Teapot_Attack.cs
@@ -4,7 +4,24 @@ using UnityEngine;
 
 public class Teapot_Attack : MonoBehaviour
 {
+    private Animator anim;
+    private bool statement;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(Delay());
+    }
     void Update()
     {
+
+    }
+    IEnumerator Delay()
+    {
+        statement = true;
+        anim.SetBool("Attack", statement == true);
+        yield return new WaitForSeconds(1);
+        anim.SetBool("Attack", statement != true);
+        yield return new WaitForSeconds(2);
+        StartCoroutine(Delay());
     }
 }
